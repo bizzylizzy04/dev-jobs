825279b67e4dc8c17be78e1f9ccf0cf9a4a03e13
Liz <bizzylizzy04@users.noreply.github.com>
2021-11-29 19:23:41 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.querySelector(".button-container")
     getJobs().then(jobs => {
         let filteredJobs = filterJobs(jobs, text);
         showJobs(filteredJobs);
-    })
+    });
 })
 
 function getJobs(){
@@ -16,48 +16,46 @@ function getJobs(){
 }
 
 function filterJobs(jobs, searchText){
-    if (searchText) {
+    if(searchText){
         let filteredJobs = jobs.filter(job => {
-          if (job.roleName.toLowerCase().includes(searchText) 
-          || job.type.toLowerCase().includes(searchText) ||
-          job.company.toLowerCase().includes(searchText) ||
-          job.requirements.content.toLowerCase().includes(searchText)) {
-            return true
-          } else {
-            return false
-          }
-        })
-        console.log(filteredJobs);
+            if(job.roleName.toLowerCase().includes(searchText) 
+            || job.type.toLowerCase().includes(searchText)
+            || job.company.toLowerCase().includes(searchText)
+            || job.requirements.content.toLowerCase().includes(searchText)){
+            return true;
+        } else {
+            return false;
+        }
+        });
         return filteredJobs;
-      } else {
+    } else {
         return jobs;
     }
 }    
 
 function showJobs(jobs){
-    console.log(jobs);
     let jobsContainer = document.querySelector(".jobs-container");
     let jobsHTML = "";
     jobs.forEach(job => {
         jobsHTML += `
-        <div class="job-tile">
-        <div class="top">
-            <img src="${job.logo}" alt="company logo">
-            <span class="material-icons more_horiz">more_horiz</span>
-        </div>
-        <div class="rolename">
-            <span>${job.roleName}</span>
-        </div>
-        <div class="description">${job.requirements.content}</div>
-        <div class="buttons">
-            <div class="button apply-now">
-                Apply Now
+            <div class="job-tile">
+            <div class="top">
+                <img src="${job.logo}" alt="company logo">
+                <span class="material-icons more_horiz">more_horiz</span>
             </div>
-            <div class="button">
-                Message
+            <div class="rolename">
+                <span>${job.roleName}</span>
+            </div>
+            <div class="description">${job.requirements.content}</div>
+            <div class="buttons">
+                <div class="button apply-now">
+                    <a href=${job.applicationLink}  target="_blank">Apply Now</a>
+                </div>
+                <div class="button">
+                <a href=${job.website}  target="_blank">Message</a>
+                </div>
             </div>
         </div>
-    </div>
     `
     });
     jobsContainer.innerHTML = jobsHTML;
